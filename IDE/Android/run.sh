2401b2c11c8613029e66e4466d3984795d7702ca
Andras Fekete <andras@wolfssl.com>
2023-12-06 12:14:37 -0500

    Remove superfluous copy to Android

--- IDE/Android/run.sh
+++ IDE/Android/run.sh
@@ -7,7 +7,16 @@ WORKSPACE=$(pwd)
 rm -rf ${WORKSPACE}/openssl-install/share
 rm -rf ${WORKSPACE}/openssl-install/ssl/misc/tsget
 
-adb push --sync ${WORKSPACE}/openssl ${WORKSPACE}/openssl-install ${WORKSPACE}/wolfssl-install/lib/libwolfssl.so ${WORKSPACE}/wolfProvider/.libs/libwolfprov.so ${WORKSPACE}/wolfProvider/provider.conf ${WORKSPACE}/wolfProvider/scripts run_helper.sh ${WORKSPACE}/wolfProvider/examples/openssl_example run_openssl.sh /data/local/tmp/.
+adb push --sync ${WORKSPACE}/openssl-install \
+        ${WORKSPACE}/openssl-source/test/evp_test \
+        ${WORKSPACE}/wolfssl-install/lib/libwolfssl.so \
+        ${WORKSPACE}/wolfProvider/.libs/libwolfprov.so \
+        ${WORKSPACE}/wolfProvider/provider.conf \
+        ${WORKSPACE}/wolfProvider/scripts \
+        ${WORKSPACE}/run_helper.sh \
+        ${WORKSPACE}/wolfProvider/examples/openssl_example \
+        ${WORKSPACE}/run_openssl.sh \
+        /data/local/tmp/.
 
 adb shell "cd /data/local/tmp/ && ./run_helper.sh"
 
