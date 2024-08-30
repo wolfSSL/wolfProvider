f77fdbe2f67de03bffa242291df5dfc5d802ba97
Andras Fekete <andras@wolfssl.com>
2024-08-30 08:44:39 -0400

    Add context structure

--- src/wp_dec_epki2pki.c
+++ src/wp_dec_epki2pki.c
@@ -31,11 +31,13 @@
 
 #include <wolfssl/wolfcrypt/asn_public.h>
 
-/* Dummy type for EPKI to PKI context. */
-typedef void wp_Epki2Pki;
-
-/* A fake static global context. */
-static unsigned char fakeCtx[1];
+/**
+ * EPKI to PKI context.
+ */
+typedef struct wp_Epki2Pki {
+    /** Provider context - useful when duplicating. */
+    WOLFPROV_CTX* provCtx;
+} wp_Epki2Pki;
 
 /**
  * Create a new EPKI to PKI context.
@@ -47,20 +49,26 @@ static unsigned char fakeCtx[1];
  */
 static wp_Epki2Pki* wp_epki2pki_newctx(WOLFPROV_CTX* provCtx)
 {
-    (void)provCtx;
-    return fakeCtx;
+    wp_Epki2Pki* ctx = NULL;
+
+    if (wolfssl_prov_is_running()) {
+        ctx = (wp_Epki2Pki*)OPENSSL_zalloc(sizeof(*ctx));
+    }
+    if (ctx != NULL) {
+        ctx->provCtx = provCtx;
+    }
+
+    return ctx;
 }
 
 /**
  * Dispose of EPKI to PKI context.
  *
- * Nothing to do as it is a global context.
- *
- * @param [in] ctx  EPKI to PKI context. Unused.
+ * @param [in] ctx  EPKI to PKI context.
  */
 static void wp_epki2pki_freectx(wp_Epki2Pki* ctx)
 {
-    (void)ctx;
+    OPENSSL_free(ctx);
 }
 
 #if LIBWOLFSSL_VERSION_HEX < 0x05000000
--- src/wp_dec_pem2der.c
+++ src/wp_dec_pem2der.c
@@ -31,36 +31,42 @@
 
 #include <wolfssl/wolfcrypt/asn_public.h>
 
-/* Dummy type for PEM to DER context. */
-typedef void wp_Pem2Der;
-
-/* A fake static global context. */
-static unsigned char fakeCtx[1];
+/**
+ * PEM to DER context.
+ */
+typedef struct wp_Pem2Der {
+    /** Provider context - useful when duplicating. */
+    WOLFPROV_CTX* provCtx;
+} wp_Pem2Der;
 
 /**
  * Create a new PEM to DER context.
  *
- * No context data required so returning a global context.
- *
- * @param [in] provCtx  Provider context. Unused.
+ * @param [in] provCtx  Provider context.
  * @return  Pointer to context.
  */
 static wp_Pem2Der* wp_pem2der_newctx(WOLFPROV_CTX* provCtx)
 {
-    (void)provCtx;
-    return fakeCtx;
+    wp_Pem2Der* ctx = NULL;
+
+    if (wolfssl_prov_is_running()) {
+        ctx = (wp_Pem2Der*)OPENSSL_zalloc(sizeof(*ctx));
+    }
+    if (ctx != NULL) {
+        ctx->provCtx = provCtx;
+    }
+
+    return ctx;
 }
 
 /**
  * Dispose of PEM to DER context.
  *
- * Nothing to do as it is a global context.
- *
- * @param [in] ctx  PEM to DER context. Unused.
+ * @param [in] ctx  PEM to DER context.
  */
 static void wp_pem2der_freectx(wp_Pem2Der* ctx)
 {
-    (void)ctx;
+    OPENSSL_free(ctx);
 }
 
 /**
