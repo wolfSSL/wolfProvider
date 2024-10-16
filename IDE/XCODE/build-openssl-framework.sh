a81a4ae791c0811a18e21fd28a36b74146f932cc
Andras Fekete <andras@wolfssl.com>
2024-10-16 17:29:21 -0400

    Update license text

--- IDE/XCODE/build-openssl-framework.sh
+++ IDE/XCODE/build-openssl-framework.sh
@@ -2,23 +2,22 @@
 
 # build-openssl-framework.sh
 #
-# Copyright (C) 2006-2023 wolfSSL Inc.
+# Copyright (C) 2006-2024 wolfSSL Inc.
 #
-# This file is part of wolfSSL.
+# This file is part of wolfProvider.
 #
-# wolfSSL is free software; you can redistribute it and/or modify
+# wolfProvider is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation; either version 3 of the License, or
 # (at your option) any later version.
 #
-# wolfSSL is distributed in the hope that it will be useful,
+# wolfProvider is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
+# along with wolfProvider. If not, see <http://www.gnu.org/licenses/>.
 
 set -euo pipefail
 
