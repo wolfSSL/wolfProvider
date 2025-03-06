512f50f37f58eb251e893b65e8dee5e4b89e2ca3
Colton Willey <colton@wolfssl.com>
2025-03-06 13:57:36 -0800

    Formatting changes per review comments

--- src/wp_rsa_kmgmt.c
+++ src/wp_rsa_kmgmt.c
@@ -961,13 +961,14 @@ static int wp_rsa_validate(const wp_Rsa* rsa, int selection, int checkType)
     return ok;
 }
 
-static int numParams(const OSSL_PARAM *p) {
+static int wp_params_count(const OSSL_PARAM *p)
+{
     int cnt = 0;
-    while(p && p->key != NULL) {
-        cnt++;
-        p++;
-    }
-    return cnt;
+    while ((p != NULL) && (p->key != NULL)) {
+         cnt++;
+         p++;
+     }
+     return cnt;
 }
 
 /**
@@ -986,7 +987,7 @@ static int wp_rsa_import_key_data(wp_Rsa* rsa, const OSSL_PARAM params[],
     int i;
     int cnt;
 
-    if (priv && (numParams(params) > 2)) {
+    if (priv && (wp_params_count(params) > 2)) {
         cnt = WP_RSA_PARAM_NUMS_CNT;
     }
     else {
