542264f1f76c2dad5c718ea67fe34ce95ba33b6e
Aidan Garske <aidan@wolfssl.com>
2025-06-17 13:33:17 -0700

    Use arch specific configs for binaries

--- scripts/utils-openssl.sh
+++ scripts/utils-openssl.sh
@@ -90,10 +90,20 @@ install_openssl() {
     if [ ! -d ${OPENSSL_INSTALL_DIR} ]; then
         printf "\tConfigure OpenSSL ${OPENSSL_TAG} ... "
         if [ "$WOLFPROV_DEBUG" = "1" ]; then
-            ./config shared enable-trace --prefix=${OPENSSL_INSTALL_DIR} --debug >>$LOG_FILE 2>&1
+            if [ "$OPENSSL_ARCH" = "linux-aarch64" ]; then
+                echo "Configuring OpenSSL for ARM64"
+                ./Configure linux-aarch64 shared enable-trace --prefix=${OPENSSL_INSTALL_DIR} --debug no-asm >>$LOG_FILE 2>&1
+            else
+                ./config shared enable-trace --prefix=${OPENSSL_INSTALL_DIR} --debug >>$LOG_FILE 2>&1
+            fi
             RET=$?
         else
-            ./config shared --prefix=${OPENSSL_INSTALL_DIR} >>$LOG_FILE 2>&1
+            if [ "$OPENSSL_ARCH" = "linux-aarch64" ]; then
+                echo "Configuring OpenSSL for ARM64"
+                ./Configure linux-aarch64 shared --prefix=${OPENSSL_INSTALL_DIR} no-asm >>$LOG_FILE 2>&1
+            else
+                ./config shared --prefix=${OPENSSL_INSTALL_DIR} >>$LOG_FILE 2>&1
+            fi
             RET=$?
         fi
         if [ $RET != 0 ]; then
