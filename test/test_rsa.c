08a8dfc61ad77af5af7e1537ef0b938878038e41
Test User <test@example.com>
2025-10-17 15:50:45 -0700

    Add correct rsa fix

--- test/test_rsa.c
+++ test/test_rsa.c
@@ -589,7 +589,7 @@ int test_rsa_sign_sha1(void *data)
     if (err == 0) {
         err = RAND_bytes(buf, sizeof(buf)) == 0;
     }
-#ifdef REPLACE_DEFAULT
+#ifndef REPLACE_DEFAULT
     if (err == 0) {
         PRINT_MSG("Sign with OpenSSL");
         err = test_digest_sign(pkey, osslLibCtx, buf, sizeof(buf), "SHA-1",
